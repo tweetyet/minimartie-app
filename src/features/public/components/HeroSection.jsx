d3dd83298b68caca6d6c5766ac71195328193846
tweetyet <ytheint80@gmail.com>
2026-04-02 08:16:45 +0700

    new

--- src/features/public/components/HeroSection.jsx
+++ src/features/public/components/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
           className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
           role="alert"
         >
-          <span className="text-xs bg-yellow-600 rounded-full text-white px-4 py-1.5 mr-3">
+          <span className="text-s bg-yellow-600 rounded-full text-white px-4 py-1.5 mr-3">
             New
           </span>{" "}
           <span className="text-sm font-medium">
