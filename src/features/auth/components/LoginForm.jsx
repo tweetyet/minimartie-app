4cafb7153b14809495b80befcc2c18b0b4846944
tweetyet <ytheint80@gmail.com>
2026-04-18 15:06:44 +0700

    final update

--- src/features/auth/components/LoginForm.jsx
+++ src/features/auth/components/LoginForm.jsx
@@ -63,7 +63,7 @@ const LoginForm = () => {
         </div>
         <a
           href="#"
-          className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+          className="text-sm font-medium text-brown hover:underline dark:text-blue-500"
         >
           Forgot password?
         </a>
@@ -71,7 +71,7 @@ const LoginForm = () => {
       <button
         type="submit"
         disabled={isSubmitting}
-        className="w-full text-white flex disabled:pointer-events-none disabled:opacity-80 justify-center items-center gap-3 bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="w-full text-white flex disabled:pointer-events-none disabled:opacity-80 justify-center items-center gap-3 bg-[#A16207] hover:bg-[#A16207] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#A16207] dark:hover:bg-[#A16207] dark:focus:ring-blue-800"
       >
         Sign in
         {isSubmitting && <ButtonSpinner />}
@@ -80,7 +80,7 @@ const LoginForm = () => {
         Don’t have an account yet?{" "}
         <Link
           to="/register"
-          className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+          className="font-medium text-brown hover:underline dark:text-blue-500"
         >
           Sign up
         </Link>
